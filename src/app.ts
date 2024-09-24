e90ef37c2b3cf6b4df2f0c844e0e4d6a5ff82563
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-09-23 21:51:13 -0500

    ADD: whitelist

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import { Response } from 'express';
-import http from 'http';
+import express from "express";
+import cors from "cors";
+import { Response } from "express";
+import http from "http";
 
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
-import routerApi from './routes';
+import routerApi from "./routes";
 
 function createApp() {
   const app = express();
   const server = http.createServer(app);
   const io = new Server(server);
 
-  const whitelist = [
-    'http://locahost:4000',
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'https://develop.fudibot-front.pages.dev',
-    'https://app.fudibot.com'
-  ]
+  const whitelist = ["http://localhost:5173"];
 
   app.use(cors({ origin: whitelist }));
 
   app.use(express.json());
 
-  app.get('/', (_req, res: Response) => {
-    res.send('generate image is aliveee')
+  app.get("/", (_req, res: Response) => {
+    res.send("generate image is aliveee");
   });
 
   routerApi(app, io);
-
 
   return { app, server };
 }
