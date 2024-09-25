576b436dbe47ef1a9bd7f193ac3035df04cc39cb
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-09-24 20:22:09 -0500

    FIX: cors and header in vercel

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { Response } from "express";
 import http from "http";
+
 import { Server } from "socket.io";
 
 import routerApi from "./routes";
@@ -11,41 +12,14 @@ function createApp() {
   const server = http.createServer(app);
   const io = new Server(server);
 
-  // Lista de dominios permitidos
-  const whitelist = ["https://alegra-frontend-challenge.vercel.app"];
+  // const whitelist = ["http://localhost:5173"];
 
-  const corsOptions = {
-    origin: (
-      origin: string | undefined,
-      callback: (err: Error | null, allowed?: boolean) => void
-    ) => {
-      if (whitelist.indexOf(origin || "") !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-
-  // Usar CORS con las opciones configuradas
-  app.use(cors(corsOptions));
-
-  // Agregar la cabecera de Permissions-Policy
-  app.use((req, res, next) => {
-    res.setHeader(
-      "Permissions-Policy",
-      "geolocation=(self), microphone=()" // Ajustar según sea necesario, excluyendo 'private-state-token-redemption'
-    );
-    next();
-  });
+  app.use(cors({ origin: "*" }));
 
   app.use(express.json());
 
   app.get("/", (_req, res: Response) => {
-    res.send("generate image is aliveee");
+    res.send("generate image is alive");
   });
 
   routerApi(app, io);
